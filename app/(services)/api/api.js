b8800ab44d07c181846245aac2f3031a36c4f0f2
DiegoDFB <difrancisblanco@gmail.com>
2024-09-12 12:41:25 -0300

    aplicativo v1

---
+++ app/(services)/api/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const loginUser = async ({email, password}) => {
+
+        const response = await axios.post(
+            "https://ripe-crabs-guess.loca.lt/api/users/login",
+            {
+                email,
+                password,
+            }
+        );
+        return response.data;
+}
+
+const registerUser = async ({email, password}) => {
+    
+    const response = await axios.post(
+        "https://ripe-crabs-guess.loca.lt/api/users/register",
+        {
+            email,
+            password,
+        }
+    );
+    return response.data;
+}
+
+export { loginUser, registerUser }
